36cb1c7abc2d56e2906e653df4b8a2803c440d80
Иван Романов <godardje@student.21-school.ru>
2024-07-09 00:44:31 +0700

    add logger

--- tests/sbispage.py
+++ tests/sbispage.py
@@ -1,6 +1,5 @@
 from baseapp import BasePage
 from selenium.webdriver.common.by import By
-#from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from loguru import logger
--- tests/test_page.py
+++ tests/test_page.py
@@ -4,27 +4,29 @@ from selenium import webdriver
 import time
 from loguru import logger
 
+logger.add("file.log", rotation="500 MB") 
 
 def test_scenario_number_one(browser):
-    logger.add("scenario_one.log", rotation="500 MB") 
+    logger.info("Тест сценария No1")
     sbis_page = SbisPage(browser)
     sbis_page.go_to_page()
     sbis_page.click_button_contacts()
-    #time.sleep(5)
     sbis_page.click_tenzor_banner()
-    #time.sleep(5)
     sbis_page.go_to_new_page()
-    #time.sleep(5)
     text = sbis_page.find_sila_v_ludiah()
     assert text == 'Сила в людях'
     sbis_page.click_tenzor_about()
-    #time.sleep(5)
     assert "https://tensor.ru/about" in sbis_page.driver.current_url
-    #time.sleep(5)
     element = sbis_page.find_working_block()
     text = element.text
     assert text == 'Работаем'
     assert sbis_page.check_img() == True
+
+
+def test_scenario_number_two(browser):
+    logger.info("Тест сценария No2")
+
+    
 
 
     
